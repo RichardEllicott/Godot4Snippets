0fa04d4135af9c7d0da5a262be5a85fa0d2f14d7
Richard Ellicott <mescalin@gmail.com>
2023-03-23 22:25:21 +0000

    .

--- snippets/get_or_create_child.gd
+++ snippets/get_or_create_child.gd
@@ -7,13 +7,13 @@ grab a node child by name, but if it doesn't exist create it and also ensure it 
 
 """
 
+## Godot 4 get_or_create_child
 static func get_or_create_child(parent: Node, node_name: String, node_type = Node) -> Node:        
-    var node = parent.get_node_or_null(node_name) # get the node if present
-    if not is_instance_valid(node): # if no node found make one
-        node = node_type.new()
-        node.name = node_name
-        parent.add_child(node)
-        if Engine.is_editor_hint(): # if we are in tool mode
-            node.set_owner(parent.get_tree().edited_scene_root) # update the actual editor scene
-#    assert(node is node_type) # best to check the type matches # does not work in Godot 4
-    return node
+    var child = parent.get_node_or_null(node_name) # get the node if present
+    if not is_instance_valid(child): # if no node found make one
+        child = node_type.new()
+        child.name = node_name
+        parent.add_child(child)
+        if Engine.is_editor_hint():
+            child.owner = parent.owner # required to spawn in editor tool mode    
+    return child
---
+++ snippets/rotate_towards_targets.gd
@@ -0,0 +1,30 @@
+"""
+
+still trying to find decent functions to rotate towards an target over a period of time at a linear speed (not interpolate spring like speed)
+
+
+best so far gets an angle from a Quaterion to scale the speed by
+
+"""
+
+
+
+static func rotate_towards_linear(from: Basis, to: Basis, delta: float, correction = true) -> Basis:
+    
+    var _return: Basis = from
+    
+    from = from.orthonormalized() # required?
+    
+    # this is the full rotation we need to make, like this it would work like a spring
+    var rotation_basis: Basis = to * from.inverse()
+    
+    # as i don't know how to make it constant any other way, i scale with inverse of the angle
+    var angle_to = from.get_rotation_quaternion().angle_to(to.get_rotation_quaternion())
+    angle_to = rad_to_deg(angle_to) / 360.0 # in full rotations (will make the speed more predictable)
+    
+    if angle_to > 0.001: # stop jitter
+        if correction:
+            delta /= angle_to # correct makes constant speed, by dividing with angle distance
+        _return *= lerp(Basis.IDENTITY, rotation_basis, delta) # the lerp is a conviniant way of scaling a Basis's magnitude
+    
+    return _return 
