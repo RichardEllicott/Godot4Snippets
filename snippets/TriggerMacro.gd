46fcbc9d2c2761c0db5e4433b24aa9566798f6b0
Richard Ellicott <mescalin@gmail.com>
2023-05-27 21:55:28 +0100

    Update TriggerMacro.gd

--- snippets/TriggerMacro.gd
+++ snippets/TriggerMacro.gd
@@ -1,5 +1,5 @@
 """
-paste in a tool script, allows triggering functions
+paste in a tool script, allows triggering functions in tool mode
 
 """
 
@@ -22,3 +22,13 @@ func _ready():
 
 func macro01():
     pass
+    
+
+# hack a bool into a trigger button that works in tool mode
+@export var trigger_update = false : set = set_trigger_update
+func set_trigger_update(input):
+    if input:
+        trigger_update = false
+        if Engine.is_editor_hint():
+            _ready() # ensure vars are available
+            # run commands here
