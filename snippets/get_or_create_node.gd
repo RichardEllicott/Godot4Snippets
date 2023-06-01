2d931fb40166e306634ee6de083c1c6156a49e11
Richard Ellicott <mescalin@gmail.com>
2023-06-01 01:38:48 +0100

    Update and rename get_or_create_child.gd to get_or_create_node.gd

--- snippets/get_or_create_node.gd
+++ snippets/get_or_create_node.gd
@@ -5,10 +5,22 @@ shortcut type function for one liners
 grab a node child by name, but if it doesn't exist create it and also ensure it is shown in editor (works in tool scripts)
 
 
-"""
+this make for really easy "autosetup" of nodes with script:
 
+var node1 = get_or_create_node(self, "node1", Node3D)
+var node2 = get_or_create_node(node1, "node2", Node3D)
+var node3 = get_or_create_node(node1, "node2", Node3D)
+var node4 = get_or_create_node(node3, "node2", Node3D)
+
+-node1
+    -node2
+    -node3
+        -node4
+
+
+"""
 ## get existing or create new child node, works in tool mode to show in editor
-func get_or_create_child(_parent, _name, type = Node3D) -> Node:
+func get_or_create_node(_parent, _name, type = Node3D) -> Node:
     var node = _parent.get_node_or_null(_name)
     if not node:
         node = type.new()
