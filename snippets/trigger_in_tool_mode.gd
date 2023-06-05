eeebffdfccfca839c2066e419c0752e50f3ae684
Richard Ellicott <mescalin@gmail.com>
2023-06-06 00:38:41 +0100

    Update trigger_in_tool_mode.gd

--- snippets/trigger_in_tool_mode.gd
+++ snippets/trigger_in_tool_mode.gd
@@ -5,6 +5,8 @@ second sample shows enumerated macros
 
 """
 
+# makes a bool work like a button in the editor (requires a tool script)
+
 @export var trigger_update: bool:
     set(value):
         if value and Engine.is_editor_hint():
@@ -12,35 +14,28 @@ second sample shows enumerated macros
             # run commands here
 
 
+# trigger some macros that run methods on the body with reflection
 
-# hack a bool into a trigger button that works in tool mode
-@export var trigger_update = false : set = set_trigger_update
-func set_trigger_update(input):
-    if input:
-        trigger_update = false
-        if Engine.is_editor_hint():
-            _ready() # ensure vars are available
-            # run commands here
+enum Macro{
+    test1,
+    test2,
+    test3
+}
 
-
-
-
-enum Macro {
-    macro01,
-   }
-
-@export var trigger_macro = false : set = set_trigger_macro
-func set_trigger_macro(input):
-    if input:
-        trigger_macro = false
-        if Engine.is_editor_hint():
-            _ready() # ensure vars are available
+@export var macro: Macro = Macro.test1: # this is my personal choice of new get/set property syntax
+    get:
+        return macro
+    set(value):
+        macro = value
         call(Macro.keys()[macro])
-@export var macro: Macro = 0
 
-func _ready():
-    pass
+func test1():
+    print("test1...")
 
-func macro01():
-    pass
+func test2():
+    print("test2...")
+    
+func test3():
+    print("test3...")
+    
     
