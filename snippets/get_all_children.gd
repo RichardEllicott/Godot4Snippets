2baeba9a665fd0312eebfe267fb2193ecd54d711
Richard Ellicott <mescalin@gmail.com>
2023-06-06 01:49:26 +0100

    Update get_all_children.gd

--- snippets/get_all_children.gd
+++ snippets/get_all_children.gd
@@ -4,10 +4,16 @@ easy way of getting all children of a node
 
 for child in get_all_children(self):
     print(child.name)
+    
+    
+using recursion is adequete for most tasks it seems
 
 """
 
-# https://godotengine.org/qa/74010/how-to-get-all-children-from-a-node
+
+## easy method using recursion, in most cases this would be fine
+## https://godotengine.org/qa/74010/how-to-get-all-children-from-a-node
+
 static func get_all_children(_self: Node, children:= []) -> Array:
     array.push_back(_self)
     for child in _self.get_children():
@@ -15,8 +21,25 @@ static func get_all_children(_self: Node, children:= []) -> Array:
     return array
     
     
-## get all children, no recursion
-## https://godotengine.org/qa/74010/how-to-get-all-children-from-a-node
+## get all children with a predicate (can use a lambda to match the children)
+## example:
+## get_all_children(self, func(child): return child is Node3D)
+
+func get_all_children(_self: Node, predicate = func (child): return child is Node):
+    var matches = []
+    var waiting := _self.get_children()
+    while waiting.size() > 0:
+        var node := waiting.pop_back() as Node
+        waiting.append_array(node.get_children())
+        if predicate.call(node):
+            matches.append(node)
+    return matches
+    
+    
+    
+    
+    
+## get all children, no recursion, no advantage but this snippet is useful
 static func get_all_children(_self: Node) -> Array:
     var children: Array = []
     var waiting := _self.get_children()
@@ -29,14 +52,22 @@ static func get_all_children(_self: Node) -> Array:
 
 
 
-## find all childs with a predicate match, default here is Node3D
-func match_children(_self: Node, predicate = func (child): return child is Node3D):
-    var matches = []
-    var waiting := _self.get_children()
-    while waiting.size() > 0:
-        var node := waiting.pop_back() as Node
-        waiting.append_array(node.get_children())
-        if predicate.call(node):
-            matches.append(node)
-    return matches
+
+    
+    
+    
+# using recursion to print a tree of the nodes
+static func print_all_children_as_tree(_self: Node, array:= [], depth = 0, max_depth = 10) -> Array:
+    
+    if depth <= max_depth:
+        
+        array.push_back(_self)
+        var pad = ""
+        for i in depth:
+            pad += "    "
+        print("%s %s" % [pad, _self])
+        
+        for child in _self.get_children():
+            array = print_all_children_as_tree(child, array, depth + 1, max_depth)
+    return array
     
