c2370213a70dc086750af52e6101ef2d745ce05f
Richard Ellicott <mescalin@gmail.com>
2023-06-06 19:26:53 +0100

    Update regex_examples.gd

--- snippets/regex_examples.gd
+++ snippets/regex_examples.gd
@@ -1,26 +1,40 @@
 """
+
+regex patterns and examples
+
+cheat sheet (perl compatible regex):
+https://www.debuggex.com/cheatsheet/regex/pcre
+
+
+
 """
 
 ## match exact string is a little off, see here:
 ## https://predictivehacks.com/?all-tips=regular-expression-for-exact-match
 
-"(?i)(?<!\w)match string(?!\w)" # matches exactly "match string"
-"^start string" # matches starting with "start string"
-"(?i)(?<!\w)_ready(?!\w)|^macro" # matches exactly "_ready" or (the | character) starts with "macro" (used for my tool macros)
+## match exactly "match string"
+"(?i)(?<!\w)match string(?!\w)"
 
+match starting with "start string"
+"^start string"
 
+## match exactly "_ready" or starts with "macro" (used for my tool macros)
+"(?i)(?<!\w)_ready(?!\w)|^macro" 
+
+## match tags "<tag1> hello world <tag2> stuff" => ["<tag1>", "<tag2>"]
+## based on: https://www.computerworld.com/article/2784456/using-regular-expressions-to-identify-xml-tags.html
+"(<.[^(><.)]+>)"
+
+## match between xml tags ??? unsure about this, also might not exclude larger matches giving bad results
+## from here https://community.splunk.com/t5/Splunk-Search/How-to-extract-a-string-from-a-particular-XML-tags-with-regular/m-p/440397
+## will sort out if i use it!
 "<tag>(?<Rmk>[^<]*)</tag>" # between xml tags like <tag>hello world</tag>
 
 
 
-## using regex to pattern match, similar to a glob:
 
-var regex: RegEx = RegEx.new()
-regex.compile("(?i)(?<!\w)_ready(?!\w)|^macro")
 
-if regex.search != null:
-    # if the search is not null, we must have a match
-    pass
+
 
 
 
