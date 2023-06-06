e3dde32921256878f9964f323d60ea254d88654d
Richard Ellicott <mescalin@gmail.com>
2023-06-06 20:54:14 +0100

    Update tag_extract.gd

--- snippets/tag_extract.gd
+++ snippets/tag_extract.gd
@@ -3,47 +3,6 @@ extract custom tags from strings, used to add instructions to blender objects, t
 
 """
 
-
-## older version, keeping if issues
-static func tag_extract(input_string: String, open_symbol: String = '<', close_symbol: String = '>') -> Array:
-    """
-    takes an input string which might have many tags like
-    "this is[tag] a sentance[end]"
-    =>
-    ["this is a sentance", "tag", "end"]
-
-    this is used to extra hidden comman tags
-
-    https://github.com/RichardEllicott/GodotSnippets/blob/master/code_patterns/tag_extract.gd
-    """
-    var nest = 0
-    var open_brackets = false
-    var text = "" # store all text not in tags
-    var tags = []
-    var bracketed_text = ""
-
-    for _char in input_string:
-        if _char == open_symbol: ## when we open the brackets
-            nest += 1
-            if nest == 1:
-                open_brackets = true
-        elif _char == close_symbol: ## when we close the brackets
-            nest -= 1
-            if nest == 0:
-                open_brackets = false
-                tags.append(bracketed_text)
-                bracketed_text = ""
-        else:
-            if open_brackets:
-                bracketed_text += _char # brackets are open record text
-            else:
-                text += _char
-    return [text] + tags
-    
-    
-    
-    
-    
 ## tag extract discreet version:
 ## tags like <tag <subtag>> will return back as "tag <subtag>"
 ## but normally throws error as max_nest = 1!
