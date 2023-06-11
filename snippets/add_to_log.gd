2573bad27f93ea9fc7eaa3800faea99dee1b76b6
Richard Ellicott <mescalin@gmail.com>
2023-06-11 22:17:35 +0100

    Update add_to_log.gd

--- snippets/add_to_log.gd
+++ snippets/add_to_log.gd
@@ -3,10 +3,17 @@ adding to a label like a console log
 
 """
 
+## allow selecting label in editor
+@export var _label: NodePath = "Label"
+var label: Node:
+    get:
+        if not label: label = get_node_or_null(_label)
+        return label
+
 var log: Array[String] = []
 var log_max: int = 100
 func add_to_log(s: String) -> void:
     log.append(s)
     if log.size() > log_max:
         log.pop_front()
-    $Label.text = "\n".join(log)
+    label.text = "\n".join(log)
