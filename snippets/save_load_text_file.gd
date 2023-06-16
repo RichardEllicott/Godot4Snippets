c2269659e14fce52d146e87b236e3b9f7f9393a9
Richard Ellicott <mescalin@gmail.com>
2023-06-16 21:13:46 +0100

    Update save_load_text_file.gd

--- snippets/save_load_text_file.gd
+++ snippets/save_load_text_file.gd
@@ -1,11 +1,11 @@
 
 
 # https://docs.godotengine.org/en/stable/classes/class_fileaccess.html
-func save_text_file(filename, content):
+func save_text_file(filename: String, content: String) -> void:
     var file = FileAccess.open(filename, FileAccess.WRITE)
     file.store_string(content)
 
-func load_text_file(filename):
+func load_text_file(filename: String) -> String:
     var file = FileAccess.open(filename, FileAccess.READ)
     var content = file.get_as_text()
     return content
