75404bed18299e4e6333dd4d484e00c6b984f967
Richard Ellicott <mescalin@gmail.com>
2023-06-16 20:27:08 +0100

    Create spawn_in_tool_mode.gd

---
+++ snippets/spawn_in_tool_mode.gd
@@ -0,0 +1,9 @@
+
+## https://docs.godotengine.org/en/stable/tutorials/plugins/running_code_in_the_editor.html
+func _ready():
+    var node = Node3D.new()
+    add_child(node) # Parent could be any node in the scene
+
+    # The line below is required to make the node visible in the Scene tree dock
+    # and persist changes made by the tool script to the saved scene file.
+    node.set_owner(get_tree().edited_scene_root)
