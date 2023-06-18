9a4ece03243942ea98f1ee9a30781acbf50c298e
Richard Ellicott <mescalin@gmail.com>
2023-06-18 03:35:18 +0100

    Update random_bytes.gd

--- snippets/random_bytes.gd
+++ snippets/random_bytes.gd
@@ -21,13 +21,14 @@ static func _get_random_bytes(count: int, seed = null) -> PackedByteArray:
         
     return bytes
 
+## example
 func macro_random_bytes():
     var byte_string = _get_random_bytes(16,4)
     print(byte_string.hex_encode()) # show as a hex string
     
     
-## adapt for password
-static func _get_random_string(count = 8, seed = null, chars: String = "0123456789abcdef"):
+# random string gets bytes also
+static func _get_random_string(count = 8, seed = null, chars: String = "0123456789abcdef") -> String:
     var rng: RandomNumberGenerator = RandomNumberGenerator.new()
     if seed:
         rng.seed = seed
@@ -35,5 +36,14 @@ static func _get_random_string(count = 8, seed = null, chars: String = "01234567
         rng.randomize()
     var s = ""
     for i in count:
-        s += chars[rng.randi() % 16]
+        s += chars[rng.randi() % chars.length()]
     return s
+
+# used for a password generator
+static func _get_random_password(count = 8, seed = null) -> String:
+    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+#    chars += "!£$%^&*()_+-=[]#{}~;':@,.<>?"
+    return _get_random_string(count, seed, chars)
+    
+    
+
