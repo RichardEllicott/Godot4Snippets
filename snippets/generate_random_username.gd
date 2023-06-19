52e633303cfd26821d36c438a496036f2f2a2cb6
Richard Ellicott <mescalin@gmail.com>
2023-06-19 04:05:43 +0100

    Update generate_random_username.gd

--- snippets/generate_random_username.gd
+++ snippets/generate_random_username.gd
@@ -4,25 +4,22 @@
 
 
 
-static func get_username_array():
-    var names_array = [
-        "Asher", "Luna", "Levi", "Stella", "Caleb", "Aurora", "Felix", "Willow",
-        "Jasper", "Ruby", "Silas", "Ivy", "Milo", "Hazel", "Ezra", "Penelope",
-        "Atticus", "Scarlett", "Sebastian", "Eleanor", "Emma", "Liam", "Olivia",
-        "Noah", "Ava", "Sophia", "Isabella", "Mia", "Jackson", "Aiden", "Lucas",
-        "Caden", "Harper", "Ethan", "Amelia", "Charlotte", "Benjamin", "Elijah",
-        "William", "James", "Ethan", "Olivia", "Liam", "Sophia", "Benjamin", "Ava",
-        "Samuel", "Isabella", "Daniel", "Mia", "Alexander", "Charlotte", "Noah", "Emma",
-        "Gabriel", "Harper", "Matthew", "Amelia", "Jameson", "Lily", "Zephyr", "Lunaire",
-        "Caspian", "Seraphina", "Magnus", "Aurora", "Orion", "Lyric", "Phoenix", "Juniper",
-        "Atlas", "Calliope", "Maverick", "Celeste", "Orion", "Aria", "Remy", "Ophelia",
-        "Finnegan", "Indira"]
-    return names_array
+var names_array = [
+    "Asher", "Luna", "Levi", "Stella", "Caleb", "Aurora", "Felix", "Willow",
+    "Jasper", "Ruby", "Silas", "Ivy", "Milo", "Hazel", "Ezra", "Penelope",
+    "Atticus", "Scarlett", "Sebastian", "Eleanor", "Emma", "Liam", "Olivia",
+    "Noah", "Ava", "Sophia", "Isabella", "Mia", "Jackson", "Aiden", "Lucas",
+    "Caden", "Harper", "Ethan", "Amelia", "Charlotte", "Benjamin", "Elijah",
+    "William", "James", "Ethan", "Olivia", "Liam", "Sophia", "Benjamin", "Ava",
+    "Samuel", "Isabella", "Daniel", "Mia", "Alexander", "Charlotte", "Noah", "Emma",
+    "Gabriel", "Harper", "Matthew", "Amelia", "Jameson", "Lily", "Zephyr", "Lunaire",
+    "Caspian", "Seraphina", "Magnus", "Aurora", "Orion", "Lyric", "Phoenix", "Juniper",
+    "Atlas", "Calliope", "Maverick", "Celeste", "Orion", "Aria", "Remy", "Ophelia",
+    "Finnegan", "Indira"]
     
 
-static func get_random_username():
-    var random_names2 = get_username_array()
-    var s = random_names2[randi() % random_names2.size()]
+func get_random_username():
+    var s = names_array[randi() % names_array.size()]
     for i in 3:
         s += str(randi() % 9)
     return s
