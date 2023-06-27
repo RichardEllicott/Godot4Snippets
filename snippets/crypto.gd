d7c6daea18bf922aa75414017f471f4dae81073b
Richard Ellicott <mescalin@gmail.com>
2023-06-27 03:13:19 +0100

    Update crypto.gd

--- snippets/crypto.gd
+++ snippets/crypto.gd
@@ -7,27 +7,26 @@ simple dressup of encryption, generates a random IV automaticly and pads to the 
 
 
 
-func simple_encrypt(data: String, key = "My secret key!!!"):
+
+func simple_encrypt(data: String, key = "My secret key!!!") -> PackedByteArray:
     
     var iv = get_random_bytes(16) # IV is 16 random bytes
     
     for i in 16 - data.length() % 16: # ensure string is 16 bytes by adding spaces
         data += " "
         
-    var aes = AESContext.new()
+    var aes: AESContext = AESContext.new()
     aes.start(AESContext.MODE_CBC_ENCRYPT, key.to_ascii_buffer(), iv)
     var encrypted = aes.update(data.to_ascii_buffer())
     aes.finish()
     return iv + encrypted # IV packed publicly to the front of bytes
 
 
-func simple_decrypt(encrypted_data: PackedByteArray, key = "My secret key!!!"):
-    
-    var random_iv = encrypted_data.slice(0, 16)
-    encrypted_data = encrypted_data.slice(16)
-    
-    var aes = AESContext.new()
-    aes.start(AESContext.MODE_CBC_DECRYPT, key.to_ascii_buffer(), random_iv)
+func simple_decrypt(encrypted_data: PackedByteArray, key = "My secret key!!!") -> String:
+    var iv: PackedByteArray = encrypted_data.slice(0, 16) # split off the iv
+    encrypted_data = encrypted_data.slice(16) # and data
+    var aes: AESContext = AESContext.new()
+    aes.start(AESContext.MODE_CBC_DECRYPT, key.to_ascii_buffer(), iv)
     var decrypted: PackedByteArray = aes.update(encrypted_data)
     aes.finish()
     return decrypted.get_string_from_utf8()
