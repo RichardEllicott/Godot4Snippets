91a7295796b8869b48cb4be348e8066e701163b1
Richard Ellicott <mescalin@gmail.com>
2023-06-27 03:11:03 +0100

    Update crypto.gd

--- snippets/crypto.gd
+++ snippets/crypto.gd
@@ -1,38 +1,54 @@
 """
 
-simple dressup of encryption, be careful as the IV is not supposed to remain secret actually it's supposed to be added to the message
-it prevents an attack that can break AES over time if the same key is used
-
-used though for casual encryption just to stop internet snoopers being clued into what the data is, or savegames it might be okay
+simple dressup of encryption, generates a random IV automaticly and pads to the front of the encrypted data
 
 """
 
 
 
-# simple encryption functions based on the example, uses CBC, the best one size fits all choice
-# IV must be of exactly 16 bytes...
-# note it is not secret, you usually pad them to the front of the message 
-# the point of the IV is to not use the same one more than once if you want to be protected from attack
-# in the case i use the encryption causually
 
-# WARNING strings as keys = bad ... this is not military grade, i only use this for basic obfusication
-
-func simple_encrypt(data: String, key = "My secret key!!!", iv = "My secret iv!!!!"):
+func simple_encrypt(data: String, key = "My secret key!!!"):
+    
+    var iv = get_random_bytes(16) # IV is 16 random bytes
+    
     for i in 16 - data.length() % 16: # ensure string is 16 bytes by adding spaces
         data += " "
+        
     var aes = AESContext.new()
-    aes.start(AESContext.MODE_CBC_ENCRYPT, key.to_utf8_buffer(), iv.to_utf8_buffer())
-    var encrypted = aes.update(data.to_utf8_buffer())
+    aes.start(AESContext.MODE_CBC_ENCRYPT, key.to_ascii_buffer(), iv)
+    var encrypted = aes.update(data.to_ascii_buffer())
     aes.finish()
-    return encrypted
+    return iv + encrypted # IV packed publicly to the front of bytes
 
 
-func simple_decrypt(encrypted_data, key = "My secret key!!!", iv = "My secret iv!!!!"):
+func simple_decrypt(encrypted_data: PackedByteArray, key = "My secret key!!!"):
+    
+    var random_iv = encrypted_data.slice(0, 16)
+    encrypted_data = encrypted_data.slice(16)
+    
     var aes = AESContext.new()
-    aes.start(AESContext.MODE_CBC_ENCRYPT, key.to_utf8_buffer(), iv.to_utf8_buffer())
-    var decrypted = aes.update(encrypted_data)
+    aes.start(AESContext.MODE_CBC_DECRYPT, key.to_ascii_buffer(), random_iv)
+    var decrypted: PackedByteArray = aes.update(encrypted_data)
     aes.finish()
-    return decrypted
+    return decrypted.get_string_from_utf8()
+    
+    
+
+func macro_test():
+    print("macro_test...")
+    
+    var key = "My secret key!!!" # Key must be either 16 or 32 bytes.
+    var data = "My secret textsss!!"
+    
+    print(data)
+    
+    data = simple_encrypt(data, key)
+    
+    print(Marshalls.raw_to_base64(data))
+    
+    data = simple_decrypt(data, key)
+    
+    print(data)
 
 
 
