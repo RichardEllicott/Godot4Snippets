ddcb3de867453bc4b1f4fd4818e3788e7bcda373
Richard Ellicott <mescalin@gmail.com>
2023-06-27 22:42:59 +0100

    Update get_or_create_node.gd

--- snippets/get_or_create_node.gd
+++ snippets/get_or_create_node.gd
@@ -7,10 +7,10 @@ grab a node child by name, but if it doesn't exist create it and also ensure it 
 
 this make for really easy "autosetup" of nodes with script:
 
-var node1 = get_or_create_node(self, "node1", Node3D)
-var node2 = get_or_create_node(node1, "node2", Node3D)
-var node3 = get_or_create_node(node1, "node2", Node3D)
-var node4 = get_or_create_node(node3, "node2", Node3D)
+var node1 = get_or_create_child(self, "node1", Node3D)
+var node2 = get_or_create_child(node1, "node2", Node3D)
+var node3 = get_or_create_child(node1, "node2", Node3D)
+var node4 = get_or_create_child(node3, "node2", Node3D)
 
 -node1
     -node2
@@ -20,7 +20,7 @@ var node4 = get_or_create_node(node3, "node2", Node3D)
 
 """
 ## get existing or create new child node, works in tool mode to show in editor
-func get_or_create_node(_parent, _name, type = Node3D) -> Node:
+func get_or_create_child(_parent, _name, type = Node3D) -> Node:
     var node = _parent.get_node_or_null(_name)
     if not node:
         node = type.new()
