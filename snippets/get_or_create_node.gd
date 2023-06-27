ceab7cb1291fe49cbd6e8a6fad599ea2322458d4
Richard Ellicott <mescalin@gmail.com>
2023-06-27 22:42:04 +0100

    Update get_or_create_node.gd

--- snippets/get_or_create_node.gd
+++ snippets/get_or_create_node.gd
@@ -33,4 +33,36 @@ func get_or_create_node(_parent, _name, type = Node3D) -> Node:
 
 
 
+## super duck typed get_or_create_child
+## _parent: target parent to add childs to
+## _name: if this is set as a string, will try to grab and return existing child (so as not to duplicate)
+##        if null, will just create a new child
+## _type: accepts a built in node like Button, Node3D etc
+##        or a string of a built in, like "Button", "Node3D"
+##        or a PackedScene
+static func get_or_create_child(_parent: Node, _name = null, _type = Node) -> Node:
+    var child
+    if _name is String:
+        child = _parent.get_node_or_null(_name)
+    if child == null:
+        if _type is PackedScene: # packed scene just instance
+            child = _type.instantiate()
+        elif _type is String:            
+            if ClassDB.class_exists(_type):
+                child = ClassDB.instantiate(_type)
+            else:
+                child = Label.new()
+                push_error("unrecognised type: \"%s\"" % _type)
+        else:
+            child = _type.new() # we assume just a plain type like Label, LineEdit or Button
+        _parent.add_child(child)
+    #    if Engine.is_editor_hint(): # i don't think this check is needed
+        child.set_owner(_parent.get_tree().edited_scene_root)
+        if _name != null:
+            child.name = _name
+    return child
+
+
+
+
 
