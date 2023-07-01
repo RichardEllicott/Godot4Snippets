00d3417b5446b1668552a16f30b7cf1bb2188eb2
Richard Ellicott <mescalin@gmail.com>
2023-07-01 22:54:44 +0100

    Update save_load_text_file.gd

--- snippets/save_load_text_file.gd
+++ snippets/save_load_text_file.gd
@@ -1,18 +1,37 @@
 """
+simple save/load data
+
+user://
+res://
+
+
 """
 
-
-static func save_text_file(filename: String, content: String) -> void:
-    var file = FileAccess.open(filename, FileAccess.WRITE)
+## standard text file
+static func save_text_file(path: String, content: String) -> void:
+    var file = FileAccess.open(path, FileAccess.WRITE)
     file.store_string(content)
 
-static func load_text_file(filename: String) -> String:
-    var file = FileAccess.open(filename, FileAccess.READ)
-    var content = file.get_as_text()
-    return content
-    
-static func save_json_file(path : String, content : Dictionary, pretty: bool = true):
+static func load_text_file(path: String) -> String:
+    var text: String = ""
+    if FileAccess.file_exists(path):
+        var file = FileAccess.open(path, FileAccess.READ)
+        text = file.get_as_text()
+    return text
+
+## json example
+static func save_json_file(path : String, content : Dictionary, pretty: bool = true) -> void:
     save_text_file(path, JSON.stringify(content, "\t"))
     
 static func load_json_file(path : String) -> Dictionary:
     return JSON.parse_string(load_text_file(path))
+    
+## godot's built in serialize (more flexible)
+static func save_var_file(path: String, content) -> void:
+    var file = FileAccess.open(path, FileAccess.WRITE)
+    file.store_var(content)
+
+static func load_var_file(path: String, content):
+    if FileAccess.file_exists(path):
+        var file = FileAccess.open(path, FileAccess.READ)
+        return file.get_var()
