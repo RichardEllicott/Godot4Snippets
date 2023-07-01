8093498685e1fe3d3f83af533c1b7c04af43052d
Richard Ellicott <mescalin@gmail.com>
2023-07-01 22:45:02 +0100

    Update save_load_text_file.gd

--- snippets/save_load_text_file.gd
+++ snippets/save_load_text_file.gd
@@ -1,11 +1,18 @@
+"""
+"""
 
 
-# https://docs.godotengine.org/en/stable/classes/class_fileaccess.html
-func save_text_file(filename: String, content: String) -> void:
+static func save_text_file(filename: String, content: String) -> void:
     var file = FileAccess.open(filename, FileAccess.WRITE)
     file.store_string(content)
 
-func load_text_file(filename: String) -> String:
+static func load_text_file(filename: String) -> String:
     var file = FileAccess.open(filename, FileAccess.READ)
     var content = file.get_as_text()
     return content
+    
+static func save_json_file(path : String, content : Dictionary, pretty: bool = true):
+    save_text_file(path, JSON.stringify(content, "\t"))
+    
+static func load_json_file(path : String) -> Dictionary:
+    return JSON.parse_string(load_text_file(path))
