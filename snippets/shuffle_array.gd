1d32bf492d67fd6fdb0a12c6148f7111dc52b143
Richard Ellicott <mescalin@gmail.com>
2023-07-16 04:20:39 +0100

    Update shuffle_array.gd

--- snippets/shuffle_array.gd
+++ snippets/shuffle_array.gd
@@ -2,19 +2,19 @@
 
 shuffle an array like shuffling cards, provide an optional seed to make the shuffle procedural
 
+leaves orginal array alone, returns a shuffled one
+
 """
 
-static func shuffle_array(list, _seed = null) -> Array:
-    list = list.duplicate()
-    if _seed:
-        seed(_seed)
+static func shuffle_array(array: Array) -> Array:
+    array = array.duplicate()
     var shuffledList = []
-    var indexList = range(list.size())
-    for i in range(list.size()):
-        var x = randi()%indexList.size()
-        shuffledList.append(list[x])
-        indexList.remove(x)
-        list.remove(x)
+    var indexList = range(array.size())
+    for i in range(array.size()):
+        var x = randi() % indexList.size()
+        shuffledList.append(array[x])
+        indexList.remove_at(x)
+        array.remove_at(x)
     return shuffledList
     
     
