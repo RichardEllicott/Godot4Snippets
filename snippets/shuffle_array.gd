0814459933481e98981a4bd421a636332faaa781
Richard Ellicott <mescalin@gmail.com>
2023-07-16 04:26:16 +0100

    Update shuffle_array.gd

--- snippets/shuffle_array.gd
+++ snippets/shuffle_array.gd
@@ -6,15 +6,14 @@ leaves orginal array alone, returns a shuffled one
 
 """
 
+
 static func shuffle_array(array: Array) -> Array:
-    array = array.duplicate()
-    var shuffledList = []
-    var indexList = range(array.size())
-    for i in range(array.size()):
-        var x = randi() % indexList.size()
-        shuffledList.append(array[x])
-        indexList.remove_at(x)
-        array.remove_at(x)
-    return shuffledList
+    var shuffled_array = [] # return this
+    array = array.duplicate() # stop affecting the orginal
+    while array.size() > 0:
+        var randi = randi() % array.size()
+        shuffled_array.append(array[randi])
+        array.remove_at(randi)
+    return shuffled_array
     
     
