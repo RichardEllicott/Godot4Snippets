1436a8b9b78eff6efb748b0d0403b3d1b07862f4
Richard Ellicott <mescalin@gmail.com>
2023-08-05 17:30:10 +0100

    Create image_drawing.gd

---
+++ snippets/image_drawing.gd
@@ -0,0 +1,12 @@
+"""
+"""
+
+
+## draw a rectangle on an image
+static func image_draw_rect(image: Image, rect: Rect2i, color: Color):
+    for _y in rect.size.y:
+        for _x in rect.size.x:        
+            var x = _x + rect.position.x
+            var y = _y + rect.position.y
+            image.set_pixel(x, y, color)
+
