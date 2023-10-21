0cf3bd37861eb9e46dec5a2422436ea20e8348e4
Richard Ellicott <mescalin@gmail.com>
2023-10-22 00:00:45 +0100

    Update lerp_array.gd

--- snippets/lerp_array.gd
+++ snippets/lerp_array.gd
@@ -11,6 +11,9 @@ this could be used to translate a position from 0 to 1 (min to max) to a curve f
 """
 
 
+var data = [0.0, 5.0, 6.0, 5.5, 5.0, 5.0]
+
+
 ## input an array of data like floats or colors
 ## get the value at a position from 0 to 1, where 0 is the start and 1 the end of the array
 func get_lerped_array(data: Array, pos: float):
@@ -27,54 +30,20 @@ func get_lerped_array(data: Array, pos: float):
     lerp_pos = lerp_pos * float(div) # a bit confusing but works
 
     return lerp(lerp1, lerp2, lerp_pos) # pos between 0-1
-
-
-
-## example:
-
-data = [0.0, 5.0, 6.0, 5.5, 5.0, 5.0]
-print(get_lerped_array(data, 0.253)
-
-
-
-
-
-
-
-## OLD OBJECT:
-
-## this object lerps a linear sequence of floats
-class LerpArray:
     
-    # a linear sequence of floats
-    var data = [0.0, 5.0, 6.0, 5.5, 5.0, 5.0]
     
-    # get a lerped value, input a pos between 0 to 1
-    # the value returned will be inbetween one of te
-    func get_lerped(pos: float):
-        
-        var div = data.size() - 1 # the total divisions is the amount of spaces in between the data values
-        
-        var seg_pos = int(pos * div) # our seg pos is the first lerp pos
-        
-        var lerp1 = data[seg_pos]
-        var lerp2 = data[seg_pos + 1]
-        
-        # we find the position between the values
-        var lerp_pos = pos - (seg_pos / float(div)) # chop off the number that is the segment
-        lerp_pos = lerp_pos * float(div) # a bit confusing but works
     
-        return lerp(lerp1, lerp2, lerp_pos) # pos between 0-1
+func macro_test():
+    
+    var divs = 128
+    
+    data = [0.0, 5.0, 6.0, 5.5, 5.0, 5.0]
         
-    func test():
+    for i in divs:
         
-        var divs = 128
-            
-        for i in divs:
-            
-            var angle = i / float(divs)
+        var angle = i / float(divs)
 
-            var polar = get_lerped(angle)
-            
-            
-            print("angle: %.2f polar: %.2f" % [angle, polar])
+        var polar = get_lerped_array(data, angle)
+        
+        
+        print("angle: %.2f polar: %.2f" % [angle, polar])
