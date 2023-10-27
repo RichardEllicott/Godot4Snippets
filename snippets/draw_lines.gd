0fee78095f109028b17b17ec4042b46cb6944a96
Richard Ellicott <mescalin@gmail.com>
2023-10-27 01:01:57 +0100

    Update draw_lines.gd

--- snippets/draw_lines.gd
+++ snippets/draw_lines.gd
@@ -1,4 +1,11 @@
 """
 quick 2D drawing
-  
-  """
+
+
+"""
+
+
+## draw an array of coordinates as lines
+func _draw_array_lines(coors: Array, color: Color = Color.WHITE, width: float = -1.0, antialiased: bool = true):
+    for i in coors.size() - 1:
+        draw_line(coors[i], coors[i + 1], color, width, antialiased)
