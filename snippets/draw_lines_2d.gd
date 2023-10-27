0d68cfe77266c2a31a9972c6ef562af3380b3a33
Richard Ellicott <mescalin@gmail.com>
2023-10-27 01:37:35 +0100

    Update draw_lines_2d.gd

--- snippets/draw_lines_2d.gd
+++ snippets/draw_lines_2d.gd
@@ -6,6 +6,11 @@ quick 2D drawing
 
 
 ## draw an array of coordinates as lines
+
+## no point! use "draw_polyline" keeping as notes incase i forget!
+
+
+
 func _draw_array_lines(coors: Array, color: Color = Color.WHITE, width: float = -1.0, antialiased: bool = true):
     for i in coors.size() - 1:
         draw_line(coors[i], coors[i + 1], color, width, antialiased)
