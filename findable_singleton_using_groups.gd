614addebba915a1ecb23371b66a09fe5a5f46360
Richard Ellicott <mescalin@gmail.com>
2023-11-08 18:56:02 +0000

    Update findable_singleton_using_groups.gd

--- findable_singleton_using_groups.gd
+++ findable_singleton_using_groups.gd
@@ -8,6 +8,13 @@ func _ready():
     add_to_group("SHMUP_GridMap", true) # put this on the findable node (or set group in editor)
 
 
+# crash if no result
+var shmup_gridmap: SHMUP_GridMap:
+    get:
+        return get_tree().get_nodes_in_group("SHMUP_GridMap")[0]
+
+
+
 var shmup_gridmap: SHMUP_GridMap:
     get:
         var ret
@@ -15,5 +22,18 @@ var shmup_gridmap: SHMUP_GridMap:
         if nodes.size() > 0:
             ret = nodes[0]
         return ret
+#    get:
+#        return get_tree().get_nodes_in_group("SHMUP_GridMap")[0]
+
+
+
+## with cache?
+var shmup_gridmap: SHMUP_GridMap:
+    get:
+        if not shmup_gridmap:
+            var nodes = get_tree().get_nodes_in_group("SHMUP_GridMap")
+            if nodes.size() > 0:
+                shmup_gridmap = nodes[0]
+        return shmup_gridmap
 #    get:
 #        return get_tree().get_nodes_in_group("SHMUP_GridMap")[0]
