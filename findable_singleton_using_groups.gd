d4849bb086505e9aba95ca9a5046271021b851fd
Richard Ellicott <mescalin@gmail.com>
2023-11-08 18:53:46 +0000

    Update findable_singleton_using_groups.gd

--- findable_singleton_using_groups.gd
+++ findable_singleton_using_groups.gd
@@ -4,15 +4,16 @@ this pattern allows finding a single node in the scene (using groups), like sing
 
 """
 
-
 func _ready():
     add_to_group("SHMUP_GridMap", true)
 
 
 var shmup_gridmap: SHMUP_GridMap:
-#    get:
-#        var nodes = get_tree().get_nodes_in_group("SHMUP_GridMap")
-#        if nodes.size() > 0:
-#            return [0]
     get:
-        return get_tree().get_nodes_in_group("SHMUP_GridMap")[0]
+        var ret
+        var nodes = get_tree().get_nodes_in_group("SHMUP_GridMap")
+        if nodes.size() > 0:
+            ret = nodes[0]
+        return ret
+#    get:
+#        return get_tree().get_nodes_in_group("SHMUP_GridMap")[0]
