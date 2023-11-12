0c71fb2c11c1a41b202c35e680ab1540fa1ef5bb
Richard Ellicott <mescalin@gmail.com>
2023-11-12 18:06:11 +0000

    Update serialize_gridmap.gd

--- snippets/serialize_gridmap.gd
+++ snippets/serialize_gridmap.gd
@@ -1,39 +1,33 @@
 """
+save a gridmap as an array of ints in the form
+
+[x1, y1, z1, item_1, orientation_1, x2, y2, z2, item_2, orientation_2 ...
+
 """
-## serialize the gridmap in the form:
-## [x, y, z, item, orientation ...
-func serialize() -> Array:
+
+
+static func serialize_gridmap(gridmap: GridMap) -> Array:
     var data = []
-    for cell in get_used_cells():
+    for cell in gridmap.get_used_cells():
         data.append(cell.x)
         data.append(cell.y)
         data.append(cell.z)
-        
-        data.append(get_cell_item(cell))
-        
-        data.append(get_cell_item_orientation(cell))
+        data.append(gridmap.get_cell_item(cell))
+        data.append(gridmap.get_cell_item_orientation(cell))
     return data
     
-func deserialize(serial_data: Array):
-    var serial_size = 5
+static func deserialize_gridmap(gridmap: GridMap, serial_data: Array) -> void:
+    var serial_size = 5 # the array is of 5 chunks
     for i in serial_data.size() / serial_size:
         var i2 = i * serial_size
-        
         var cell = Vector3(
             serial_data[i2 + 0],
             serial_data[i2 + 1],
             serial_data[i2 + 2],
             )
-
         var item = serial_data[i2 + 3]
         var orientation = serial_data[i2 + 4]
-        
-        set_cell_item(cell, item, orientation)
-    
-
-
-
-
+        gridmap.set_cell_item(cell, item, orientation)
 
 static func save_to_json_file(path : String, content : Dictionary, pretty: bool = true):
     var json_string = JSON.stringify(content, "  ")
@@ -47,19 +41,15 @@ static func load_from_json_file(path : String) -> Dictionary:
     content = JSON.parse_string(content)
     return content
 
-
-
-
-
 var filename = "user://save_data.dat"
 func macro_save_data():
-    
-    var dict = {"serial_data": serialize()}
+    var dict = {"serial_data": serialize_gridmap(self)}
     save_to_json_file(filename, dict)
 
 func macro_load_data():
-    
     var data = load_from_json_file(filename)
     var serial_data = data['serial_data']
-    deserialize(serial_data)
+    deserialize_gridmap(self, serial_data)
 
+func macro_clear_data():
+    clear()
