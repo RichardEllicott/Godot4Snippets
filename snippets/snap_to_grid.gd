9efa2811331e3d94c035b374234ccaf8c61e59b5
Richard Ellicott <mescalin@gmail.com>
2023-11-15 20:22:02 +0000

    Update snap_to_grid.gd

--- snippets/snap_to_grid.gd
+++ snippets/snap_to_grid.gd
@@ -1,4 +1,11 @@
 """
+
+not there is already a function on Vector3: "snapped"
+
+https://docs.godotengine.org/en/stable/classes/class_vector3.html#class-vector3-method-snapped
+
+so i prob don't need this
+
 """
 
 
