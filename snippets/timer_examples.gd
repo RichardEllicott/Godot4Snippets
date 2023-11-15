846705bb9f5d60be2a08222776c82a7158247701
Richard Ellicott <mescalin@gmail.com>
2023-11-15 04:53:38 +0000

    Update timer_examples.gd

--- snippets/timer_examples.gd
+++ snippets/timer_examples.gd
@@ -1,7 +1,20 @@
 """
 """
 
+## simple timer
+var timer = 0.0
+@export var delay = 1.0
+@export var random_delay = -1
 
+func _process(delta):
+    timer += delta
+    if timer >= delay:
+        timer -= delay
+        
+        if random_delay > 0.0:
+            delay = randf() * random_delay
+
+## ticker timer
 var timer = 0.0
 @export var delay = 1.0
 var ticker = 0
@@ -14,14 +27,4 @@ func _process(delta):
         print(ticker)
 
 
-var timer = 0.0
-@export var delay = 1.0
-@export var random_delay = -1
 
-func _process(delta):
-    timer += delta
-    if timer >= delay:
-        timer -= delay
-        
-        if random_delay > 0.0:
-            delay = randf() * random_delay
