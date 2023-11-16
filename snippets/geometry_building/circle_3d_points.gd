be1854e19dfb054bd89143dcdeb21189fd5fb80b
Richard Ellicott <mescalin@gmail.com>
2023-11-16 22:57:37 +0000

    Update circle_3d_points.gd

--- snippets/geometry_building/circle_3d_points.gd
+++ snippets/geometry_building/circle_3d_points.gd
@@ -5,17 +5,19 @@ method for getting circle points in 3D space
 """
 
 
+
 ## get circle points in 3d space, circle facing z
 ## build the transform with an origin and basis
-## for radius scale the basis:
+## for radius scale the basis (NOT WORKING USE radius):
 ## transform3d.basis = transform3d.basis.scaled(Vector3(1,1,1) * 2)
-static func circle_points(transform3d: Transform3D, div: int) -> Array[Vector3]:
+static func circle_3d_points(transform3d: Transform3D, div: int, radius: float = 1.0) -> Array[Vector3]:
     var points: Array[Vector3] = []
     for i in div:
         var theta = deg_to_rad(float(i) / float(div) * 360.0)
         var pos = transform3d.basis.x * cos(theta) # get circle equation offsets
         pos += transform3d.basis.y * sin(theta)
-        pos *= transform3d.basis.get_scale() # scale circle using the basis
+#        pos *= transform3d.basis.get_scale() # scale circle using the basis
+        pos *= radius
         pos += transform3d.origin # offset the position to the transform.origin
         points.append(pos)
     return points
