3af187155982fc0085e64f555a9f00f951a3c11a
Richard Ellicott <mescalin@gmail.com>
2023-11-17 01:57:02 +0000

    Update mouse_plane_position.gd

--- snippets/mouse_plane_position.gd
+++ snippets/mouse_plane_position.gd
@@ -10,8 +10,8 @@ get_viewport().get_mouse_position()
 
 ## convert the mouse position (which is on the screen) to a world position on a plane
 ## default plane normal faces up (+y)
-func get_mouse_plane_position(camera: Camera3D, mouse_position: Vector2, plane = Plane(Vector3(0,1,0))):
+static func get_mouse_plane_position(camera: Camera3D, mouse_position: Vector2, plane = Plane(Vector3(0,1,0))):
     # https://godotengine.org/qa/25922/how-to-get-3d-position-of-the-mouse-cursor
     var from = camera.project_ray_origin(mouse_position)
     var to = camera.project_ray_normal(mouse_position)
-    return plane.intersects_ray(from, to)
+    return plane.intersects_ray(from, to)     
