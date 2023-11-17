42681242197d374589c06ab877dba47ed2dc3ee5
Richard Ellicott <mescalin@gmail.com>
2023-11-17 01:45:39 +0000

    Update shape_and_raycast.gd

--- snippets/shape_and_raycast.gd
+++ snippets/shape_and_raycast.gd
@@ -29,3 +29,17 @@ func raycast(from: Vector3, to: Vector3, exclude: Array = []):
 #    query.collide_with_areas = false
 #    query.collide_with_bodies = true
     return get_world_3d().get_direct_space_state().intersect_ray(query)
+
+
+func raycast_mouse_position(cam: Camera3D):
+    
+    var space_state = get_world_3d().direct_space_state
+
+    var mousepos = get_viewport().get_mouse_position()
+
+    var origin = cam.project_ray_origin(mousepos)
+    var end = origin + cam.project_ray_normal(mousepos) * RAY_LENGTH
+    var query = PhysicsRayQueryParameters3D.create(origin, end)
+    query.collide_with_areas = true
+    var result = space_state.intersect_ray(query)
+    return result
