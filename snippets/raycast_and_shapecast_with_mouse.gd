6d44d4859c16cb696a845b5efbe628091c06dd84
Richard Ellicott <mescalin@gmail.com>
2023-11-17 19:25:53 +0000

    Update raycast_and_shapecast_with_mouse.gd

--- snippets/raycast_and_shapecast_with_mouse.gd
+++ snippets/raycast_and_shapecast_with_mouse.gd
@@ -43,3 +43,14 @@ func raycast_mouse_position(cam: Camera3D):
     query.collide_with_areas = true
     var result = space_state.intersect_ray(query)
     return result
+
+
+# this is similar to a raycast
+
+## convert the mouse position (which is on the screen) to a world position on a plane
+## default plane normal faces up (+y)
+static func get_mouse_plane_position(camera: Camera3D, mouse_position: Vector2, plane = Plane(Vector3(0,1,0))):
+    # https://godotengine.org/qa/25922/how-to-get-3d-position-of-the-mouse-cursor
+    var from = camera.project_ray_origin(mouse_position)
+    var to = camera.project_ray_normal(mouse_position)
+    return plane.intersects_ray(from, to)     
