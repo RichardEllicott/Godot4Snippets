fda3e41fb9ddd252b93c0408335b5acf9e086aad
Richard Ellicott <mescalin@gmail.com>
2023-11-24 21:47:01 +0000

    Update serialize.gd

--- snippets/serialize.gd
+++ snippets/serialize.gd
@@ -1,36 +1,41 @@
 """
-easy saving and loading
+example of easy saving and loading, no json problems just using Godot's systems
 """
 
-
+## save filename
 @export var save_path = "user://platform_land_terrain.dat"
-#@export var save_path = "user://score.save"
 
+## test vars
+var a = "hello"
+var b = "world"
+var c = "again"
+
+## list of vars to serialize
 var serialize_vars = [
-    "grid_dimensions",
-    "cliff_heights",
-    "doodad_map"
+    "a",
+    "b",
+    "c"
 ]
 
-
+## serialize the vars to a dictionary
 func serialize() -> Dictionary:
     var data: Dictionary = {}
     for _var in serialize_vars:
         data[_var] = get(_var)
     return data
     
-    
+## deserialize the vars from a dictionary
 func deserialize(data: Dictionary):
     for _var in serialize_vars:
         set(_var, data[_var])
 
-
+## save the vars to a file
 func save_data():
     var file = FileAccess.open(save_path, FileAccess.WRITE)
     file.store_var(serialize())
-    
+
+## restore the vars from a file
 func load_data():
     if FileAccess.file_exists(save_path):
         var file: FileAccess = FileAccess.open(save_path, FileAccess.READ)
         deserialize(file.get_var())
-        _build_terrain()
