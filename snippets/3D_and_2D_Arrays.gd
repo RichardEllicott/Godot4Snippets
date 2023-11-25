4ea0b46328c5ae4fdefdb722a5f21c60bf832b3c
Richard Ellicott <mescalin@gmail.com>
2023-11-25 21:28:10 +0000

    Update 3D_and_2D_Arrays.gd

--- snippets/3D_and_2D_Arrays.gd
+++ snippets/3D_and_2D_Arrays.gd
@@ -6,6 +6,7 @@ however it is a bit of a pain to set up, especially in 3D, this objects demos a 
 
 """
 
+
 ## a 3d array
 ## internally uses one giant long array, even a packed one
 ## this makes it more effecient than nested arrays
@@ -18,76 +19,92 @@ however it is a bit of a pain to set up, especially in 3D, this objects demos a 
 ## WARNING, using 3 dimensions can use a crazy amount of memory!
 class Array3D:
     
-    var size: Vector3i # dimensions of the 3D array
+    var _size: Vector3i # dimensions of array (private)
+    var _array # the array itself (private)
     
-    var array # can be array or packed array like PackedInt32Array
+    ## setting the size may trigger the array to have to copy (slow)
+    var size: Vector3i:
+        get:
+            return _size
+        set(val):
+            set_size(val)
     
     ## resize entire array (expensive)
-    ## will copy all the data accross if we increase the array size
-    ## will crop data that doesn't fit (delete)
-    ## empty cells will get a default value like null or 0 depending on array type
-    func resize(new_size: Vector3i):
+    ## will copy all data it can, drop data that doesn't fit   
+    func set_size(new_size: Vector3i):
         
-        var new_data = array.duplicate() # duplicate (will ensure same array type)
-        new_data.clear() # clear it as the order no longer makes sense
-        new_data.resize(new_size.x * new_size.y * new_size.z) # set to new size
-        
-        # copy area is smallest common area   
-        var copy_area = size             
-        if copy_area.x > new_size.x:
-            copy_area.x = new_size.x
-        if copy_area.y > new_size.y:
-            copy_area.y = new_size.y
-        if copy_area.z > new_size.z:
-            copy_area.z = new_size.z
-        
-        # copy all the data accross 
-        for z in copy_area.z:
-            for y in copy_area.y:
-                for x in copy_area.x:
-                    var i = x + y * size.x + z * size.x * size.y # old array ref
-                    var i2 = x + y * new_size.x + z * new_size.x * new_size.y #  new array ref
-                    new_data[i2] = array[i]
-                    
-        array = new_data
-        size = new_size
+        if new_size != _size: # only resize if we change the size
+            
+            var new_data = _array.duplicate() # duplicate (will ensure same array type)
+            new_data.clear() # clear it as the order no longer makes sense
+            new_data.resize(new_size.x * new_size.y * new_size.z) # set to new size
+            
+            # copy area is smallest common area   
+            var copy_area = _size             
+            if copy_area.x > new_size.x:
+                copy_area.x = new_size.x
+            if copy_area.y > new_size.y:
+                copy_area.y = new_size.y
+            if copy_area.z > new_size.z:
+                copy_area.z = new_size.z
+            
+            # copy all the data accross 
+            for z in copy_area.z:
+                for y in copy_area.y:
+                    for x in copy_area.x:
+                        var i = x + y * _size.x + z * _size.x * _size.y # old array ref
+                        var i2 = x + y * new_size.x + z * new_size.x * new_size.y #  new array ref
+                        new_data[i2] = _array[i]
+                        
+            _array = new_data
+            _size = new_size
     
     ## we can optionally pass an array like PackedInt32Array()
-    func _init(_size: Vector3i, _array = []):
-                
-        size = _size
-        array = _array
+    func _init(size, array = []):
         
-        var target_size = size.x * size.y * size.z
+        if size is Vector2i:
+            size = Vector3i(size.x, size.y, 1)
+        assert(size is Vector3i)
+                
+        _size = size
+        _array = array
+        
+        var target_size = _size.x * _size.y * _size.z
         
         if array.size() == 0: # typically the array size should be 0 at this point
             array.resize(target_size) # this will crash if not a type of Array
         else:
             ## we can pass in a filled array if we like
-            ## but it MUST have the correct dimensions
+            ## this assert stops the wrong dimensions being passed in
             assert(array.size() == target_size)
             
     ## get the value at a position, for 2D, use z=0
     func get_value(pos: Vector3i):
-        var i = pos.x # inline to avoid passing value types
-        i += pos.y * size.x
-        i += pos.z * size.x * size.y
-        return array[i]
+        var i = pos.x + pos.y * _size.x + pos.z * _size.x * _size.y # inline
+        return _array[i]
         
     func set_value(pos: Vector3i, value):
-        var i = pos.x # inline to avoid passing value types
-        i += pos.y * size.x
-        i += pos.z * size.x * size.y
-        array[i] = value
+        var i = pos.x + pos.y * _size.x + pos.z * _size.x * _size.y # inline
+        _array[i] = value
+    
+    ## 2D functions assume z to be 0
+    func get_value_2d(pos: Vector2i):
+        var i = pos.x + pos.y * _size.x # inline
+        return _array[i]
+        
+    ## 2D functions assume z to be 0
+    func set_value_2d(pos: Vector2i, value):
+        var i = pos.x + pos.y * _size.x # inline
+        _array[i] = value
     
     ## gets a string to visualize the array
     ## the string is actually the same serial array but arranged to look like floors
     ## string can therefore be pasted back into godot
     func get_string() -> String:
         var s = ""
-        for z in size.z: # for all floors
-            for y in size.y: # for all rows   
-                for x in size.x: # add a csv style row
+        for z in _size.z: # for all floors
+            for y in _size.y: # for all rows   
+                for x in _size.x: # add a csv style row
                     s += "%s, " % get_value(Vector3i(x,y,z))
                 s += "\n"
             s += "\n" # seperate z floors with a gap        
@@ -103,7 +120,7 @@ func macro_test_array3d():
     array3d.set_value(Vector3i(2, 1, 0), 4)
     print("ARRAY:")
     print(array3d.get_string())
-    array3d.resize(Vector3i(4,4,2))
+    array3d.size = Vector3i(4,4,2)
     print("ARRAY:")
     print(array3d.get_string())
     
@@ -114,6 +131,6 @@ func macro_test_array3d():
     array3d.set_value(Vector3i(2, 1, 0), 4)
     print("ARRAY:")
     print(array3d.get_string())
-    array3d.resize(Vector3i(4,4,2))
+    array3d.size = Vector3i(4,4,2)
     print("ARRAY:")
     print(array3d.get_string())
