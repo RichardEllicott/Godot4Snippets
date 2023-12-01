1f08115fa34d5795419c614549883a5e3875f893
Richard Ellicott <mescalin@gmail.com>
2023-12-01 20:55:20 +0000

    Update get_or_create_child.gd

--- snippets/get_or_create_child.gd
+++ snippets/get_or_create_child.gd
@@ -43,34 +43,54 @@ static func clear_children(_self: Node):
 
 
 
-## super duck typed get_or_create_child
+
+
+
+## the following function is used to simplify instantiating controls
+
+## super duck typed version of my standard get_or_create_child function
+##
+## either returns the existing node, or spawns a new one (including in tool mode)
+##
 ## _parent: target parent to add childs to
 ## _name: if this is set as a string, will try to grab and return existing child (so as not to duplicate)
 ##        if null, will just create a new child
 ## _type: accepts a built in node like Button, Node3D etc
 ##        or a string of a built in, like "Button", "Node3D"
 ##        or a PackedScene
-static func get_or_create_child(_parent: Node, _name = null, _type = Node) -> Node:
-    var child
-    if _name is String:
-        child = _parent.get_node_or_null(_name)
-    if child == null:
-        if _type is PackedScene: # packed scene just instance
+##        or a plain Node that we will duplicate (warning children and data might be lost on the duplicate)
+static func get_or_create_child(_parent: Node, _name: String, _type = Node) -> Node:
+    var child = _parent.get_node_or_null(_name) # attempt to get the node by name
+    if child == null: # if no node, we need to create one
+        
+        # if a packed scene just instance it
+        if _type is PackedScene: 
             child = _type.instantiate()
-        elif _type is String:            
+            
+        # if a string, we can try to match it in the ClassDB
+        # this works for built in types and user scripts with class_name
+        elif _type is String: 
             if ClassDB.class_exists(_type):
                 child = ClassDB.instantiate(_type)
             else:
-                child = Label.new()
-                push_error("unrecognised type: \"%s\"" % _type)
+                child = Label.new() # unrecognised type string
+                push_error("unrecognised type string: \"%s\"" % _type)
+        
+        # if we put in a plain node
+        # unlike a PackedScene, this typically will not duplicate children and may possibly loose data
+        elif _type is Node:
+            child = _type.duplicate() # warning using duplicate may be less effecient
+        
+        # the last assumption we make is we have typed a plain type, this works like:
+        # get_or_create_child(self, "Name", Button)
         else:
             child = _type.new() # we assume just a plain type like Label, LineEdit or Button
+        child.name = _name
+        
         _parent.add_child(child)
-    #    if Engine.is_editor_hint(): # i don't think this check is needed
-        child.set_owner(_parent.get_tree().edited_scene_root) # NEW
-        if _name != null:
-            child.name = _name
-    return child
+        # we always set the owner after adding the node to a tree if using tool mode
+        child.owner = _parent.get_tree().edited_scene_root 
 
+    return child
 
 
