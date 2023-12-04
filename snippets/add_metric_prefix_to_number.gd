2742d599963e5b3ff0a77423228047b1f2e9e7d3
Richard Ellicott <mescalin@gmail.com>
2023-12-04 22:02:00 +0000

    Update add_metric_prefix_to_number.gd

--- snippets/add_metric_prefix_to_number.gd
+++ snippets/add_metric_prefix_to_number.gd
@@ -14,25 +14,24 @@ this function takes a number and reduces it to the best metric prefix plus an op
 ## 1100 => 1.1 k
 ## 1000000 => 1 M
 ## 1234000 => 1.234 M
-static func metric_prefix(n: float, decimal_places: int = 3, suffix = "W"):
+static func metric_prefix(n: float, decimal_places: int = 3, suffix = "W") -> String:
     
     #https://en.wikipedia.org/wiki/Metric_prefix
     var symbols = "kMGTPEZYRQ" # positive symbols
     
     var symbols2 = "mμnpfazyrq" # for later, the fractions like mili, micro, nano, pico
     
-    var symbol = ""
+    var symbol = "" # we put the symbol here if applicable (n >= 1000)
     
     var s = str(abs(n)).split('.') # make a split of the positive number to count the characters
     
     var chunks_count = int((s[0].length() - 1) / 3.0) # the total chunks of 3 past 1 number, ie 1000000 is 6 zeros, 2 chunks
     
-    if chunks_count > 0:
+    if chunks_count > 0: # if we have at least one chunk we will add a symbol
         symbol = symbols[chunks_count - 1]
-        
-        n /= 1000 ** chunks_count
+        n /= 1000 ** chunks_count # divide the number by 1000, 1000000, 1000000000 etc
     
-    n = snapped(n, 0.1 ** decimal_places) # snap to decimal places
+    n = snapped(n, 0.1 ** decimal_places) # snap to decimal places (leaves no trailing zeros)
     
-    return "%s %s%s" % [n, symbol, suffix]
+    return "%s %s%s" % [n, symbol, suffix] # return a string
         
