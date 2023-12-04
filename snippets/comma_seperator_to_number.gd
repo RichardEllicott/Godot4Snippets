7f3beff8f57f916656418fed083b97ad7f375396
Richard Ellicott <mescalin@gmail.com>
2023-12-04 00:24:42 +0000

    Update comma_seperator_to_number.gd

--- snippets/comma_seperator_to_number.gd
+++ snippets/comma_seperator_to_number.gd
@@ -5,15 +5,15 @@
 """
   
   
+
 # https://ask.godotengine.org/18559/how-to-add-commas-to-an-integer-or-float-in-gdscript
-static func comma_sep(n: int) -> String:
+# added sep option
+static func comma_sep(n: int, sep: String = ",") -> String:
     var result := ""
     var i: int = abs(n)
     
-    var mode = 1
-
-    while i > 999:
-        result = ",%03d%s" % [i % 1000, result]
+    while i >= 1000:
+        result = "%s%03d%s" % [sep, i % 1000, result]
         i /= 1000
-
+        
     return "%s%s%s" % ["-" if n < 0 else "", i, result]
