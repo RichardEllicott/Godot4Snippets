0500e1758dbb16e01c826d8a452b032f368cd63f
Richard Ellicott <mescalin@gmail.com>
2023-12-05 19:24:24 +0000

    Update normal_probability_distribution_function.gd

--- snippets/normal_probability_distribution_function.gd
+++ snippets/normal_probability_distribution_function.gd
@@ -9,8 +9,8 @@ this gives the height y of a bell curve where we are at the position x
 
 # ported normal probability distribution function
 # https://stackoverflow.com/questions/12412895/how-to-calculate-probability-in-a-normal-distribution-given-mean-standard-devi
-func normal_pdf(x: float, mean: float = 0.0, sd: float = 1.0) -> float:
-    var v = float(sd)**2
-    var denom = (2 * PI * v)**.5
-    var num = exp(-(float(x)-float(mean))**2 / (2 * v))
+static func normal_pdf(x: float, mean: float = 0.0, standard_deviation: float = 1.0) -> float:
+    var v: float = float(standard_deviation)**2
+    var denom: float = (2 * PI * v)**.5
+    var num: float = exp(-(float(x)-float(mean))**2 / (2 * v))
     return num / denom
