3456171fa49d1e042ea7af9c39324e6dd49675a5
Richard Ellicott <mescalin@gmail.com>
2023-12-05 19:45:44 +0000

    Update normal_function_and_pdf.gd

--- snippets/normal_function_and_pdf.gd
+++ snippets/normal_function_and_pdf.gd
@@ -25,33 +25,29 @@ i have these though as one extra thing can be done, and Godot has no "probabilit
 
 
 """
-## this is my old 
-func normal_2d(r1 = randf(), r2 = randf()):
-    """
-    ported from the caltech lua one (i think, the link is dead!)
-    http://www.design.caltech.edu/erik/Misc/Gaussian.html
-    
-    https://www.taygeta.com/random/gaussian.html # new link
-    
-    https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
 
-    r1 and r2 are floats from 0 to 1
 
-    ran_gaussian_2D(randf(),randf())
-    """
-    var al1 = sqrt(-2 * log(r1)) # part one
-    var al2 = 2 * PI * r2 # part two
-    var x = al1 * cos(al2)
-    var y = al1 * sin(al2)
+## ported from the caltech lua one (i think, the link is dead!)
+## http://www.design.caltech.edu/erik/Misc/Gaussian.html
+## https://www.taygeta.com/random/gaussian.html # new link
+## https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+## r1 and r2 are floats from 0.0 to 1.0
+func normal_2d(r1 = randf(), r2 = randf()) -> Vector2:
+    var al1: float = sqrt(-2.0 * log(r1)) # part one
+    var al2: float = 2.0 * PI * r2 # part two
+    var x: float = al1 * cos(al2)
+    var y: float = al1 * sin(al2)
     return Vector2(x,y)
 
 
-#gives the height y of a bell curve where we are at the position x
-# ported normal probability distribution function
-# https://stackoverflow.com/questions/12412895/how-to-calculate-probability-in-a-normal-distribution-given-mean-standard-devi
+## normal probability distribution function
+## gives the height y of a bell curve where we are at the position x
+## ported from python:
+## https://stackoverflow.com/questions/12412895/how-to-calculate-probability-in-a-normal-distribution-given-mean-standard-devi
 static func normal_pdf(x: float, mean: float = 0.0, standard_deviation: float = 1.0) -> float:
     var v: float = float(standard_deviation)**2
     var denom: float = (2 * PI * v)**.5
     var num: float = exp(-(float(x)-float(mean))**2 / (2 * v))
     return num / denom
+    
 
