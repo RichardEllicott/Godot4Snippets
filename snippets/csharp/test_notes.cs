5585f854591dc57ca59b100e67a766cb722f8853
Richard Ellicott <mescalin@gmail.com>
2024-04-12 03:10:32 +0100

    Update test_notes.cs

--- snippets/csharp/test_notes.cs
+++ snippets/csharp/test_notes.cs
@@ -1,12 +1,10 @@
 
 
-// this test shows how static properties are shared
-static Godot.Collections.Dictionary<Node3D, bool> _test_if_we_are_shared = new Godot.Collections.Dictionary<Node3D, bool>();public void test_if_we_are_shared(){
-    _test_if_we_are_shared[this] = true;
-    GD.Print("test_if_we_are_shared: ", _test_if_we_are_shared);
+// static objects are like singletons in C#, i like to take advantage of this
 
-    foreach (var entry in _test_if_we_are_shared)
-    {
-        GD.Print("-entry: ", entry);
-    }
-}
+
+// the delay syntax, i need a string
+await ToSignal(GetTree().CreateTimer(1.0/32.0), "timeout");
+
+
+
