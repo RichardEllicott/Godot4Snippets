289362b4434e08988d96e377cef7a3b7fe7c24e7
Richard Ellicott <mescalin@gmail.com>
2024-04-17 22:36:40 +0100

    Update and rename BitFlagExtension.cs to InsertSort.cs

--- snippets/csharp/InsertSort.cs
+++ snippets/csharp/InsertSort.cs
@@ -1,18 +1,6 @@
 
 static class Helper{
 
-    // easy set bit flags on an integer
-    public static int SetBit(this int i, int position, bool value)
-    {
-        if (value) return i |= (1 << position); // set bit to 1
-        else return i & ~(1 << position);   // set bit to 0
-    }
-
-    public static bool GetBit(this int value, int position)
-    {
-        return (value & (1 << position)) != 0; // get bit at position
-    }
-
     
 
     // sort using a "straight insertion" method, does not return a re-ordered array but an array of indexes like:
