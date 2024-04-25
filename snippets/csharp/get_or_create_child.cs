84470f7cda56e5c42b56879dafde98aac2b35892
Richard Ellicott <mescalin@gmail.com>
2024-04-25 02:35:11 +0100

    Update get_or_create_child.cs

--- snippets/csharp/get_or_create_child.cs
+++ snippets/csharp/get_or_create_child.cs
@@ -1,25 +1,27 @@
 
 
 
-      // get or create child works like:
-      // var multimesh_instance3d = this.GetOrCreateChild<MultiMeshInstance3D>("MultiMeshInstance3D");
-      // called on a Node
-  public static T GetOrCreateChild<T>(this Godot.Node _parent, string _name) where T : Godot.Node, new()
-  {
-      var child = _parent.GetNodeOrNull(_name);
+// get or create child works like:
+// var multimesh_instance3d = this.GetOrCreateChild<MultiMeshInstance3D>("MultiMeshInstance3D");
+// called on a Node
+//
+// this extension should be put in a public static class, it can then be called from any Godot.Node
 
-      if (child == null)
-      {
-          child = new T();// as Godot.Node;
+    public static T GetOrCreateChild<T>(this Godot.Node _parent, string _name) where T : Godot.Node, new()
+    {
+        var child = _parent.GetNodeOrNull(_name); // check for child
 
-          child.Name = _name;
-          _parent.AddChild(child);
-          if (Engine.IsEditorHint())
-          {
-              child.Owner = _parent.GetTree().EditedSceneRoot;
-          }
-      }
-      return child as T;
-  }
+        if (child == null) // if no child
+        {
+            child = new T(); // create a new one
 
+            child.Name = _name; // set it's name
+            _parent.AddChild(child); // add as child
+            if (Engine.IsEditorHint()) // if in editor
+            {
+                child.Owner = _parent.GetTree().EditedSceneRoot; // set the owner, this makes spawned nodes show up in tool scripts
+            }
+        }
+        return child as T;
+    }
 
