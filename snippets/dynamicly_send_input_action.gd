97bd0efb141fa3afda0d96a04b16e245b34e0ab6
Richard Ellicott <mescalin@gmail.com>
2024-05-13 00:32:25 +0100

    Update dynamicly_send_input_action.gd

--- snippets/dynamicly_send_input_action.gd
+++ snippets/dynamicly_send_input_action.gd
@@ -4,11 +4,12 @@
 ## you could, it is a bit unwise i will build a singleton maybe
   
 func send_input_action(action = "ui_left", pressed = true):
-    var event = InputEventAction.new()
+
+    if not InputMap.has_action(action): ## dynamicly add if no action
+        InputMap.add_action(action)
+
+    var event = InputEventAction.new() ## create the event
     event.action = action
     event.pressed = pressed
     
-    if not InputMap.has_action(action): ## dynamicly add if no action
-        InputMap.add_action(action)
-        
     Input.parse_input_event(event) # press the action
