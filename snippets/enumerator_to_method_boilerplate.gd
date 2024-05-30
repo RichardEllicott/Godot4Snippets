0db39a5059cde432fdb52d803cce1ec6b2e5e31c
Richard Ellicott <mescalin@gmail.com>
2024-05-30 11:57:07 +0100

    Update enumerator_to_method_boilerplate.gd

--- snippets/enumerator_to_method_boilerplate.gd
+++ snippets/enumerator_to_method_boilerplate.gd
@@ -32,3 +32,33 @@ func DEFAULT():
     pass
     
 var method: Callable = DEFAULT
+
+
+
+
+## this is just an example of how a state machine could work, in this case we are using threads which sort of occurs after we call (await wait(delay))
+## now the loop is basicly a lost thread (as there is a while loop)
+## as we are unable to find this thread (very lazy), we will just break out of the loop if our state changed
+##
+## i like this syntax as it's very easy to write
+##
+func SEARCHING():
+    
+    print("start SEARCHING...")
+
+    var i = 0    
+    while true: # loop
+        if not method == SEARCHING: # if no longer function pointed method
+            break # break out of this thread!
+        i += 1
+        
+        print("searching ", i)
+        
+        var delay = get_new_delay()
+        print("wait for %.2f seconds" % delay)
+        
+        await wait(delay)
+        
+        parent.input_direction = get_random_direction()
+        
+
