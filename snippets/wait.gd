710fad0e9a08a4ae63692f0172596403ee078576
Richard Ellicott <mescalin@gmail.com>
2024-05-30 11:33:41 +0100

    Update wait.gd

--- snippets/wait.gd
+++ snippets/wait.gd
@@ -1,12 +1,14 @@
 """
 
-the wait syntax that works best
+the wait syntax that works best is:
+
+await get_tree().create_timer(1.0).timeout ## waits for the signal to fire
 
 """
 
 
-
-func wait(time: float = 1.0):
+## dress up
+func wait(time: float = 1.0) -> Signal:
     return get_tree().create_timer(time).timeout
     
     
