e26d78b6fb77983a6842af41f868cd99c63d0218
Richard Ellicott <mescalin@gmail.com>
2024-06-03 15:59:16 +0100

    Update balance_weigth_dictionary.gd

--- snippets/balance_weigth_dictionary.gd
+++ snippets/balance_weigth_dictionary.gd
@@ -1,6 +1,9 @@
 ## take a dict of numbers
 ## ensure that all the numbers add up to 1.0
 ## works on dict in place (modifys the dict)
+##
+## used to decide say the results of a lootbox, wth differing probabilities for rewards
+
 static func balance_weight_dictionary(_dict: Dictionary) -> void:
     
     var total: float = 0.0
