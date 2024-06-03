19b52aa3b70ad793ac067c47fe298c4788a094ab
Richard Ellicott <mescalin@gmail.com>
2024-06-03 22:26:50 +0100

    Update signals.gd

--- snippets/signals.gd
+++ snippets/signals.gd
@@ -1,12 +1,16 @@
 """
 """
-child.pressed.connect(_on_button_pressed.bind(child))
 
+## normal syntax
+child.pressed.connect(_on_button_pressed.bind(child))
+child.pressed.connect(_on_button_pressed.bind(1234)) # binds a par here
+
+
+## connect signal checking the signal is not already connected (as it throws an error)
+##
+## example (with bind):
+## connect_signal(Button.pressed, _on_button_pressed.bind(1234))
+##
 static func connect_signal(from: Signal, to: Callable) -> void:
     if not from.is_connected(to):
         from.connect(to)
-        
-
-
-## shows binding a parameter, in this case 1234 to the be called on the method
-child.pressed.connect(_on_button_pressed.bind(1234))
