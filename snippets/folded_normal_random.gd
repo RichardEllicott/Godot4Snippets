63cb61b9b2d1b934234828cba46d73432337bc8f
Richard Ellicott <mescalin@gmail.com>
2024-06-03 21:29:05 +0100

    Create folded_normal_random.gd

---
+++ snippets/folded_normal_random.gd
@@ -0,0 +1,6 @@
+
+## folded normal with an average output of 1
+## the folded normal only returns positive numbers
+## the constant used is (2.0/pi)**0.5
+func folded_normal():    
+    return abs(randfn(0.0, 1.0)) / 0.7978845608028654
