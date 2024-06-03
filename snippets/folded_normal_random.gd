befed78367f87ca54bb3e47d5057d3e94c657324
Richard Ellicott <mescalin@gmail.com>
2024-06-03 22:40:54 +0100

    Update folded_normal_random.gd

--- snippets/folded_normal_random.gd
+++ snippets/folded_normal_random.gd
@@ -2,5 +2,8 @@
 ## folded normal with an average output of 1
 ## the folded normal only returns positive numbers
 ## the constant used is (2.0/pi)**0.5
-func folded_normal():    
+##
+## good for delays like lightning
+##
+static func folded_normal() -> float:    
     return abs(randfn(0.0, 1.0)) / 0.7978845608028654
