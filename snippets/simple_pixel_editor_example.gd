ef2cd519840f84d6df182759ad6ee7fb1ac41fb9
Richard Ellicott <mescalin@gmail.com>
2024-06-17 20:15:16 +0100

    .

--- snippets/simple_pixel_editor_example.gd
+++ snippets/simple_pixel_editor_example.gd
@@ -7,8 +7,6 @@ will draw and update when we draw over the TextureRect's area
 
 uses the transform, so supports scaling the TextureRect
 
-
-
 """
 @tool
 extends Control
@@ -16,6 +14,7 @@ extends Control
 ## get our TextureRect, make sure there is one!
 var texture_rect: TextureRect:
     get:
+        #return self
         if not texture_rect:
             texture_rect = $TextureRect
             #texture_rect = StaticLib6.get_or_create_child(self, "TextureRect", TextureRect)
@@ -34,6 +33,7 @@ var image: Image:
             
             if not texture_rect.texture: # if no texture either create one
                 image = Image.create(image_size.x, image_size.y, true, Image.FORMAT_RGBA8)
+                image.fill(Color.DARK_GREEN)
                 var texture: ImageTexture = ImageTexture.create_from_image(image)
                 texture_rect.texture = texture
             
@@ -43,6 +43,7 @@ var image: Image:
         ## TODO, RESIZE IMAGE??
         if image.get_size() != image_size: # if image size now wrong, create a new image
             image = Image.create(image_size.x, image_size.y, true, Image.FORMAT_RGBA8)
+            image.fill(Color.DARK_GREEN)
         
         return image
             
@@ -71,18 +72,33 @@ func global_position_to_image_position(_position: Vector2) -> Vector2:
     
     return image_position
 
+var drawing = false
 
 
+## uses unhandled_input
+## if it is not working, make sure all the controls node are set to mouse pass (not stop)
+## using mouse filtering like this blocks drawing if we click a button above the draw area
+func _unhandled_input(event):
+    
+    if event.is_action(draw_action):
+        if event.is_pressed():
+            if global_position_to_image_position(mouse_position) != Vector2(-1,-1):
+                drawing = true
+        else:
+            drawing = false
+
+
+var mouse_position: Vector2 = Vector2.ZERO
     
 func _process(delta) -> void:
     
     if Engine.is_editor_hint(): # skip following in tool mode (tool mode i just use for testing)
         return
     
-    #var mouse_position: Vector2 = get_viewport().get_camera_2d().get_global_mouse_position()
-    var mouse_position: Vector2 = get_viewport().get_mouse_position()
+    #mouse_position = get_viewport().get_camera_2d().get_global_mouse_position()
+    mouse_position = get_viewport().get_mouse_position()
     
-    if Input.is_action_pressed(draw_action): # tool modes doesn't like custom event names
+    if drawing: # tool modes doesn't like custom event names
         var draw_position: Vector2 = global_position_to_image_position(mouse_position)
         
         if draw_position != Vector2(-1,-1):
