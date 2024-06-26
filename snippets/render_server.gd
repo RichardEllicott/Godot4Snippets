b4beb862376e6953ac8fab15c985c62a2144d1f2
Richard Ellicott <mescalin@gmail.com>
2024-06-26 11:48:19 +0100

    Create render_server.gd

---
+++ snippets/render_server.gd
@@ -0,0 +1,17 @@
+func _exit_tree():        
+    clear_mesh_instances()
+    
+
+var rids: Array[RID] = []
+
+func add_mesh_instance(_mesh: Mesh, _transform: Transform3D) -> RID:
+    
+    var rid: RID = RenderingServer.instance_create2(_mesh, get_world_3d().scenario)
+    RenderingServer.instance_set_transform(rid, _transform)
+    rids.append(rid)
+    return rid
+    
+func clear_mesh_instances():
+    for rid: RID in rids:
+        RenderingServer.free_rid(rid)
+    rids = []
