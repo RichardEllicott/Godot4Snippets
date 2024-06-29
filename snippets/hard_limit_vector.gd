ef50cce45e44b0dc84b770c9aecbd8ba1dbc4593
Richard Ellicott <mescalin@gmail.com>
2024-06-29 18:23:06 +0100

    Update hard_limit_vector.gd

--- snippets/hard_limit_vector.gd
+++ snippets/hard_limit_vector.gd
@@ -7,27 +7,31 @@ the ignore functions are useful if we limit say the x,z velocity but ignore the 
 """
 
 
+
 ## hard limit a vector, means it's length cannot exceed a certain length
 ## the ignore will ignore that axis, ie ignore y to ignore the gravity velocity
 static func hard_limit_vector(
-    input: Vector3,
+    vector: Vector3, # he
     limit: float,
     ignore_x: bool = false,
     ignore_y: bool = false,
     ignore_z: bool = false
     ):
-    
-    var ret: Vector3 = input
-    
-    if ignore_x: ret.x = 0.0
-    if ignore_y: ret.y = 0.0
-    if ignore_z: ret.z = 0.0
-    
-    if ret.length() > limit:
-        ret = ret.normalized() * limit
-    
-    if ignore_x: ret.x = input.x
-    if ignore_y: ret.y = input.y
-    if ignore_z: ret.z = input.z
         
-    return ret
+    # create return vector
+    var new_vector: Vector3 = vector
+    
+    # 0 the ignored axis
+    if ignore_x: new_vector.x = 0.0 
+    if ignore_y: new_vector.y = 0.0
+    if ignore_z: new_vector.z = 0.0
+    
+    # if longer than limit set to limit
+    if new_vector.length() > limit: new_vector = new_vector.normalized() * limit 
+    
+    # put back ignored axis
+    if ignore_x: new_vector.x = vector.x 
+    if ignore_y: new_vector.y = vector.y
+    if ignore_z: new_vector.z = vector.z
+        
+    return new_vector
