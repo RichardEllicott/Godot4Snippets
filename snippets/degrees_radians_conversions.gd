541d146de5d4e986c0b67f1c11769aec766516fb
Richard Ellicott <mescalin@gmail.com>
2024-06-29 17:08:53 +0100

    Update degrees_radians_conversions.gd

--- snippets/degrees_radians_conversions.gd
+++ snippets/degrees_radians_conversions.gd
@@ -16,3 +16,21 @@ static func vec2_deg_to_rad(input: Vector2) -> Vector2:
     #
 static func vec2_rad_to_deg(input: Vector2) -> Vector2:
     return Vector2(rad_to_deg(input.x),rad_to_deg(input.y))
+
+
+
+## example property, showing setting in degrees but the code will use the radians (simplfies the underlying code to not convert values)
+
+@export var rotation_speed: Vector3 = Vector3(0.0, 0.5, 0.0)
+
+@export var rotation_speed_degreees: Vector3:
+    get:
+        return Vector3(
+            rad_to_deg(rotation_speed.x),
+            rad_to_deg(rotation_speed.y),
+            rad_to_deg(rotation_speed.z)
+            )
+    set(value):
+        rotation_speed.x = deg_to_rad(value.x)
+        rotation_speed.y = deg_to_rad(value.y)
+        rotation_speed.z = deg_to_rad(value.z)
